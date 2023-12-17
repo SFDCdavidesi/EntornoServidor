a0e1a033df5d853afaa59c39b10edc2de3fb0ea2
David <david@herrero.us>
2023-12-17 23:11:04 +0100

    solucionado problema con index.php

--- ejercicio13diciembre/index.php
+++ ejercicio13diciembre/index.php
@@ -21,34 +21,36 @@ require_once("php/header.php");
             require_once("php/muestralibros.php");
             ?>
         </div>
-        <?php
-        if (isset($resultadoaccion)) {
-            include "php/mensajeacciones.php";
-        }
-        ?>
+
         <div class="row">
             <div class="col-6">
                 <button type="button" class="btn btn-info" onclick="muestra('formulariolibros');">Crear</button>
 
             </div>
         </div>
-    
-    <div class="row d-none" id="formulariolibros">
-        <div class="col">
         <?php
+        if (isset($resultadoaccion)) {
+            include "php/mensajeacciones.php";
+        }
+        ?>
+
+        <div class="row d-none" id="formulariolibros">
+            <div class="col">
+                <?php
         include("php/formularioLibros.php");
         ?>
+            </div>
         </div>
     </div>
     <?php
-    if (isset($_REQUEST["verFormulario"]) && $_REQUEST["verFormulario"] == "modificarLibro") {
+     if (isset($_REQUEST["verFormulario"]) && $_REQUEST["verFormulario"] == "modificarLibro") {
         $codigolibro = $_REQUEST["codigo_libro"];
     ?>
-        <div class="container " id="formulariolibrosmodificar">
-            <?php
+    <div class="container " id="formulariolibrosmodificar">
+        <?php
             include("php/formularioLibrosModificar.php");
             ?>
-        </div>
+    </div>
     <?php
     }
     ?>
